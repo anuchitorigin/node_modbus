92a0508d39962cba6b19a857fa03b6de2f9e84f9
Anuchit <anuchit.b@origin55.com>
2025-05-28 10:38:46 +0700

    update random number generation

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import { Server } from 'net'
 
 const netServer = new Server()
 const initialCoils = Buffer.alloc(10000)
-const initialHoldingRegisters = Buffer.alloc(200)
+const initialHoldingRegisters = Buffer.alloc(200) // bytes for 100 registers
 const server = new Modbus.server.TCP(netServer, {
   coils: initialCoils,
   holding: initialHoldingRegisters
@@ -13,7 +13,7 @@ server.on('connection', function (client) {
   console.log('['+(new Date())+'] New Connection');
 })
 
-// server.on('readCoils', function (request, response, send) {
+// server.on('preReadCoils', function (request, response, send) {
 //   /* Implement your own */
 
 //   response.body.coils[0] = true
@@ -22,11 +22,15 @@ server.on('connection', function (client) {
 //   send(response)
 // })
 
-// server.on('readHoldingRegisters', function (request, response, send) {
-
-//   /* Implement your own */
-
-// })
+server.on('preReadHoldingRegisters', function (response: any, send: any) {
+  console.log('Pre Read Holding Registers')
+  /* Implement your own */
+  for (let i = 0; i < 32; i++) {
+    const randomValue = Math.floor(Math.random() * 5000)
+    console.log(`Setting register ${i} to random value: ${randomValue}`)
+    server.holding.writeUInt16BE(randomValue, i)
+  }
+})
 
 // server.on('preWriteSingleRegister', function (value, address) {
 //   console.log('Write Single Register')
@@ -60,8 +64,22 @@ server.coils.writeUInt16BE(0x0001, 6)
 
 server.discrete.writeUInt16BE(0x5678, 0)
 
-server.holding.writeUInt16BE(0x0000, 0)
-server.holding.writeUInt16BE(0x0000, 2)
+server.holding.writeUInt16BE(0x0010, 0)
+server.holding.writeUInt16BE(0x0180, 1)
+server.holding.writeUInt16BE(0x0710, 2)
+server.holding.writeUInt16BE(0x0101, 3)
+server.holding.writeUInt16BE(0x0611, 4)
+server.holding.writeUInt16BE(0x0110, 5)
+server.holding.writeUInt16BE(0x1510, 6)
+server.holding.writeUInt16BE(0x1100, 7)
+server.holding.writeUInt16BE(0x1010, 8)
+server.holding.writeUInt16BE(0x1410, 9)
+server.holding.writeUInt16BE(0x1011, 10)
+server.holding.writeUInt16BE(0x0301, 11)
+server.holding.writeUInt16BE(0x1011, 12)
+server.holding.writeUInt16BE(0x0210, 13)
+server.holding.writeUInt16BE(0x0010, 14)
+server.holding.writeUInt16BE(0x0110, 15)
 
 server.input.writeUInt16BE(0xff00, 0)
 server.input.writeUInt16BE(0xff00, 2)
